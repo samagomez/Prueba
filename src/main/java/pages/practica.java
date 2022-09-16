10b7c920961f362f481ed3f8c63098f0cc638858
salvador gomez <smg581986@gmail.com>
2022-09-15 20:39:03 -0600

    NSYN

--- src/main/java/pages/practica.java
+++ src/main/java/pages/practica.java
@@ -2,23 +2,7 @@ package pages;
 
 public class practica { //aqui estoy
 	
-	//GOAOAOAOAOA
-	//repositorio
-	//PROBLEMAS NUEVOS
-	//buenos dias
-	// a ver que ondas mi amiguix
-
-	//subiendo cambios aqui 
-	//cuando no se sabe que hacer
+	
 }//valores 
 
-//BENDECIDO
-//local
-//macdonald
-//PADRE RICO
-//PADRE POBRE
-// CAMBIOS 
 
-
-// MIS NUEVOS NUEVOS
-//LLLLLLLLLLLLLLLLLL
